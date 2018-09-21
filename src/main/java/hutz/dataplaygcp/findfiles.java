52c7f50c02a535420d521a8a6825bea9cd6c52a6
Timothy Hutz <timothyhutz@gmail.com>
2018-09-20 23:34:01 -0500

    Updated pom and findobjects done.

--- src/main/java/hutz/dataplaygcp/findfiles.java
+++ src/main/java/hutz/dataplaygcp/findfiles.java
@@ -1,20 +1,32 @@
 package hutz.dataplaygcp;
 
+import com.google.api.gax.paging.Page;
+import com.google.cloud.storage.Blob;
 import com.google.cloud.storage.Bucket;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
 
+import javax.swing.text.html.HTMLDocument;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Properties;
 
 
 public class findfiles {
-    public static List databuckets() {
-        List<String> objectlist = new ArrayList<String>();
+    public static List databuckets() throws IOException {
+        List<Blob> objectlist = new ArrayList<Blob>();
         Storage storageclient = StorageOptions.getDefaultInstance().getService();
+        String bucketname = getPropertyvalues().getProperty("BUCKET_NAME");
+        Page<Blob> rootobjects = storageclient.list(bucketname);
+        Iterator<Blob> rootobjectsiterator = (Iterator<Blob>) rootobjects.iterateAll();
+        while (rootobjectsiterator.hasNext()) {
+            Blob rootobject = rootobjectsiterator.next();
+            objectlist.add(rootobject);
+        }
         return objectlist;
     }
 
--- src/main/java/hutz/dataplaygcp/main.java
+++ src/main/java/hutz/dataplaygcp/main.java
@@ -1,7 +1,10 @@
 package hutz.dataplaygcp;
 
+import java.io.IOException;
+
 public class main {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
         System.out.println("Hey People Load System");
+        System.out.println(findfiles.databuckets());
     }
 }
